36a01d5b3994b6864ce6eafe54787e6c13710227
Crowdbotics <team@crowdbotics.com>
2023-05-10 18:04:42 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/bold_frog_40612/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bold_frog_40612;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "bold_frog_40612";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bold-frog-40612",
+  url: "https://bold-frog-40612.botics.co",
+  api: "https://bold-frog-40612.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
